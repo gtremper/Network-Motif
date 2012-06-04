31fd46c72deeb9d3b59d7edd6a76b5615cdafaee
Graham Tremper <gtremper@gmail.com>
2012-06-04 16:41:45 -0700

    Convert ID works now

--- GraphParse.py
+++ GraphParse.py
@@ -1,4 +1,6 @@
 import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
 import os
  
 #Out put a text file representation of graph
@@ -16,9 +18,23 @@ def outputGraph(graph,name="OUTPUT.txt"):
 def findMotifs(G,motifSize):
 	outputGraph(G)
 	os.system("./Kavosh -i result/OUTPUT.txt -r 1000 -s "+str(motifSize))
+
 	
-	
+def convertIDToGraph(id,motifSize):
+	binary = bin(id);
+	adj = np.zeros(motifSize*motifSize)
+	for x in xrange(motifSize*motifSize):
+		if binary[-x] == 'b':
+			break
+		adj[-x] = int(binary[-x])
+	adj.shape = (motifSize,motifSize)
+	M = np.mat(adj)
+	graph = nx.from_numpy_matrix(M,create_using=nx.DiGraph())
+	nx.draw(graph)
+	plt.show()
 
 if __name__ == '__main__':
-	G = nx.gn_graph(100)
-	findMotifs(G,4)
+	#G = nx.gn_graph(100)
+	#findMotifs(G,4)
+	convertIDToGraph(642,4)
+	
