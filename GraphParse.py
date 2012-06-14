9c804d1cfdae57d6c1da6fad8d81e38e454ed85b
Graham Tremper <gtremper@gmail.com>
2012-06-14 13:56:31 -0700

    working on motif ordering

--- GraphParse.py
+++ GraphParse.py
@@ -5,6 +5,8 @@ import sys
 import os
 import cPickle as pickle
 import scipy.stats as stats
+
+USECACHE = True
  
 #Out put a text file representation of graph
 def outputGraph(graph,name="OUTPUT.txt"):
@@ -34,15 +36,12 @@ def convertIDToGraph(id,motifSize):
 	nx.draw_circular(graph)
 	#plt.savefig("result/id-"+str(id)+"size-"+str(motifSize))
 	plt.show()
-	
-
-
 
 # Main finding motifs routine	
 def findMotifs(data,key,motifSize,degree,usetotal=False):
 	#Check cache
 	filename = str(key)+'s'+str(int(motifSize))+'d'+str(int(degree))+str(usetotal)+".pkl"
-	if os.path.exists('cache/'+filename):
+	if os.path.exists('cache/'+filename) and USECACHE:
 		print "in cache"
 		with open('cache/'+filename,"rb") as f:
 			return pickle.load(f)
@@ -70,73 +69,108 @@ def findMotifs(data,key,motifSize,degree,usetotal=False):
 		#Jenky way to use c++ motif finder in python
 		os.system("./Kavosh "+str(motifSize))
 		data = np.loadtxt("result/MotifCount.txt",ndmin=2)
+		
 		for iD,total,percent in data:
 			if iD in motifs:
-				motifs[iD].append(total if usetotal else percent)
+				motifs[int(iD)].append(total if usetotal else percent)
 			else:
-				motifs[iD] = [total if usetotal else percent]
-	
-	print '\nMotifs Done!'
+				motifs[int(iD)] = [total if usetotal else percent]
 		
+		
+	print '\nMotifs Done!'
+	
+	#add zeros to graphs that didn't contain motifs
 	for key,value in motifs.iteritems():
 		numZero = len(graphs)-len(value)
 		value.extend([0 for derp in xrange(numZero)])
-		motifs[key] = np.array(value)
+		motifs[int(key)] = np.array(value)
 	
 	#add motifs to cache
-	with open('cache/'+filename,'wb') as f:
-		pickle.dump(motifs,f)
+	if USECACHE:
+		with open('cache/'+filename,'wb') as f:
+			pickle.dump(motifs,f)
 		
 	return motifs
 
-def aveDegree(G):
-	return G.size()/float(len(G))	
+def motifOrder(data,key,orderSize,motifSize,degree):			
+	graphs = data[key]
+	pattern = {}
+	for G in graphs:
+		#calculate threshold
+		sortedWeights = np.sort(G,axis=None)
+		threshold = sortedWeights[-len(G)*degree-1]
+		#Output graph to txt file
+		graph = nx.DiGraph(G>threshold)
+		graph = nx.convert_node_labels_to_integers(graph,1)
+		with open('result/OUTPUT.txt','wb') as f:
+			f.write(str(len(graph)) + '\n')
+			nx.write_edgelist(graph,f,data=False)
+		#Jenky way to use c++ motif finder in python
+		os.system("./Kavosh "+str(motifSize))
+		data = np.loadtxt("result/MotifCount.txt",ndmin=2)
+		
+		order = []
+		for iD,total,percent in data:
+			order.append((iD,total))
+		keys = sorted(order,key=lambda x:-x[1])
+		keys = [int(k[0]) for k in keys]
+		pat = tuple(keys[:orderSize])
+		pattern[pat] = pattern.setdefault(pat,0) + 1/float(len(graphs))
+		
+		
+	total = sorted(pattern.items(), key = lambda x: -x[1])
+	
+	for key,value in total:
+		print str(key)+": " + str(value)
 
-def statTest(data,motifSize,degree):
+		
+
+def motifStats(data,motifSize,degree, usetotal=False):
+	
 	for corr in ('corr','lcorr','lacorr'):
-		motifsNL = findMotifs(data,('NL',corr), motifSize, degree)
-		motifsMCI = findMotifs(data,('MCI',corr), motifSize, degree)
-		motifsAD = findMotifs(data,('AD',corr), motifSize, degree)
+		motifsNL = findMotifs(data,('NL',corr), motifSize, degree, usetotal)
+		motifsMCI = findMotifs(data,('MCI',corr), motifSize, degree, usetotal)
+		motifsAD = findMotifs(data,('AD',corr), motifSize, degree, usetotal)
 		
-		allMotifs = list(set(motifsNL.keys()) & set(motifsAD.keys()) & set(motifsMCI.keys()))
+		allMotifs = list(set(motifsNL.keys()) | set(motifsAD.keys()) | set(motifsMCI.keys()))
 		
-		filename = "result/{0} ks-stats size-{1} deg-{2}".format(corr,motifSize,degree)
+		datatype = "Total" if usetotal else "Percent"
+		filename = "result/{}_ks-stats_size-{}_deg-{}.txt".format(corr+datatype,motifSize,degree)
 		with open(filename,'w') as f:
 			f.write("{0:>10}{1:>15}{2:>15}{3:>15}{4:>15}{5:>15}\n".format('ID','MCI','AD','NORM NL','NORM MCI','NORM AD'))
 			for key in allMotifs:
-				KSstatistic, MCIpvalue = stats.ks_2samp(motifsMCI[key],motifsNL[key])
-				KSstatistic, ADpvalue = stats.ks_2samp(motifsAD[key],motifsNL[key])
-				k2,NLnorm = stats.normaltest(motifsNL[key])
-				k2,MCInorm = stats.normaltest(motifsMCI[key])
-				k2,ADnorm = stats.normaltest(motifsAD[key])
-				sig = "*" if (MCIpvalue<0.01 or ADpvalue <0.01) else ''
-				
-				line = "{0:>10}{1:15.3}{2:15.3}{3:15.3}{4:15.3}{5:15.3}\n"
-				f.write(line.format(sig+str(int(key)),MCIpvalue,ADpvalue,NLnorm,MCInorm,ADnorm))
+				NLdata = motifsNL.get(key,np.zeros(88))
+				MCIdata = motifsMCI.get(key,np.zeros(88))
+				ADdata = motifsAD.get(key,np.zeros(88))
+				KSstatistic, MCIpvalue = stats.ks_2samp(MCIdata,NLdata)
+				KSstatistic, ADpvalue = stats.ks_2samp(ADdata,NLdata)
+				k2,NLnorm = stats.normaltest(NLdata)
+				k2,MCInorm = stats.normaltest(MCIdata)
+				k2,ADnorm = stats.normaltest(ADdata)
+				if MCIpvalue<0.01 or ADpvalue<0.01:
+					line = "*{0:>9}{1:15.3}{2:15.3}{3:15.3}{4:15.3}{5:15.3}\n"
+				else:
+					line = "{0:>10}{1:15.3}{2:15.3}{3:15.3}{4:15.3}{5:15.3}\n"
+				f.write(line.format(str(int(key)),MCIpvalue,ADpvalue,NLnorm,MCInorm,ADnorm))
 
-				
-			
-			
-
-
-		
-def plotMotifGraphs(data,motifSize,degree,numofmotifs):
+					
+def plotMotifGraphs(data,motifSize,degree,numofmotifs,usetotal=False):
 	for corr in ('corr','lcorr','lacorr'):
-		nl=findMotifs(data,('NL',corr), motifSize, degree)
-		mci=findMotifs(data,('MCI',corr), motifSize, degree)
-		ad=findMotifs(data,('AD',corr), motifSize, degree)
+		nl=findMotifs(data,('NL',corr), motifSize, degree,usetotal)
+		mci=findMotifs(data,('MCI',corr), motifSize, degree,usetotal)
+		ad=findMotifs(data,('AD',corr), motifSize, degree,usetotal)
 		
 		motifs = nl.items()
 		motifs = sorted(motifs,key=lambda x:-x[1].mean())
 		keys = [int(x[0]) for x in motifs[:numofmotifs]]
+		
 		meansNL = []
 		meansMCI = []
 		meansAD = []
 		stdNL = []
 		stdMCI = []
 		stdAD = []
-		for key in keys:
-			key = float(key)
+		for key in keys:			
 			if key in nl:
 				meansNL.append(nl[key].mean())
 				stdNL.append(nl[key].std())
@@ -170,7 +204,10 @@ def plotMotifGraphs(data,motifSize,degree,numofmotifs):
 		plt.ylim(ymin=0.0)
 		plt.legend( (NLplt[0], MCIplt[0], ADplt[0]), ('NL', 'MCI', 'AD') )
 		plt.grid(True)
-		plt.savefig("result/MotifDistTotal-"+corr+" D-"+str(degree)+" S-"+str(motifSize))
+		if usetotal:
+			plt.savefig("result/TotalMotifDist-"+corr+"_D-"+str(degree)+"_S-"+str(motifSize))
+		else:
+			plt.savefig("result/PercentMotifDist-"+corr+"_D-"+str(degree)+"_S-"+str(motifSize))
 		plt.clf()
 	
 
@@ -179,11 +216,13 @@ if __name__ == '__main__':
 		data = pickle.load(f)
 		
 	#plotMotifGraphs(data,3,10,10)
+	print 'Normal'
+	motifOrder(data,('NL','corr'),4 , 3, 10)
+	print 'MCI'
+	motifOrder(data,('MCI','corr'),4, 3, 10)
+	print 'AD'
+	motifOrder(data,('AD','corr'),4, 3, 10)
 	
-	statTest(data,3,15)
-	statTest(data,4,10)
-	statTest(data,4,15)
-	#convertIDToGraph(142,3)
 	
 	
 
