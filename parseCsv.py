20854d80299c8614b5f1048861d306e184f4f4e2
jason <jason@jason-Ideapad-Z570.(none)>
2012-07-12 13:52:30 -0700

    reparsed the patient data

--- parseCsv.py
+++ parseCsv.py
@@ -4,6 +4,7 @@ import math
 import cPickle as pickle
 from collections import defaultdict
 import string
+import copy
 
 GRAPHSIZE = 88
 
@@ -39,13 +40,15 @@ def createCorrMatrixes(measuredict):
 	r_f = []
 	for i in xrange(GRAPHSIZE):
 		if measuredict['1'][i] == 'NA' or measuredict['3'][i] == 'NA':
-			r_e.append(-1.0)
+			return False
+            #r_e.append(-1.0)
 		else:
 			rate = float(measuredict['1'][i])/float(measuredict['3'][i])
 			rate = max(min(rate,2),0.5)
 			r_e.append(rate)
 		if measuredict['3'][i] == 'NA' or measuredict['5'][i] == 'NA':
-			r_f.append(-1.0)
+			return False
+            #r_f.append(-1.0)
 		else:
 			rate = float(measuredict['3'][i])/float(measuredict['5'][i])
 			rate = max(min(rate,2),0.5)
@@ -54,7 +57,7 @@ def createCorrMatrixes(measuredict):
 	corrmat = np.zeros(shape=(GRAPHSIZE, GRAPHSIZE))
 	for i, early in enumerate(r_e):
 		for j, final in enumerate(r_f):
-			if early == -1.0 or final == -1.0:
+			if i == j or early == -1.0 or final == -1.0:
 				corrmat[i][j] = 0
 			else:
 				corrmat[i][j] = 1 - abs(early - final)/float(early + final)
@@ -77,8 +80,49 @@ def createCorrMatrixes(measuredict):
 	
 	return (corrmat, lcorrmat, lacorrmat)
 
+def tresh(G):
+    sorted_weights = np.sort(G, axis=None)
+    threshold = sorted_weights[-GRAPHSIZE*10-1]
+    return np.matrix(G>threshold)
+    
 		
-		
+def createFinalData():
+    data = {}
+    pdd = parse('outVolumes2.csv')
+    for patientname, patientdata in pdd.iteritems():
+        patienttype = patientdata[0]
+        if patienttype in ['NL', 'AD', 'MCI', 'MCI to AD']:
+            if patienttype == 'MCI to AD':
+                patienttype = 'CONVERT'
+        else:
+            continue
+        corrmatrixes = createCorrMatrixes(patientdata[1])
+        if corrmatrixes == False:
+            continue
+        tcorrmatrixes = [tresh(x) for x in corrmatrixes]
+#            for i in xrange(GRAPHSIZE):
+#                for j in xrange(GRAPHSIZE):
+#                    if matrix[i][j] > threshold:
+#                        matrix[i][j] = 1
+#                    else:
+#                        matrix[i][j] = 0
+        matrixdict = {}
+        corr, lcorr, lacorr = corrmatrixes
+        tcorr, tlcorr, tlacorr = tcorrmatrixes
+        matrixdict['corr'] = corr
+        matrixdict['lcorr'] = lcorr
+        matrixdict['lacorr'] = lacorr
+        matrixdict['tcorr'] = tcorr
+        matrixdict['tlcorr'] = tlcorr
+        matrixdict['tlacorr'] = tlacorr
+        data[patientname] = ({}, matrixdict, patienttype)
+        
+    with open('aznorbert_corrsd_new.pkl','wb') as f:
+        pickle.dump(data,f)
+    
+    return data
+
+                               
 def createData():
 	data = defaultdict(list)
 	pdd = parse('outVolumes2.csv')
