03bb72d7e8f5c0186c625e1db504a4c5bf57ceec
Graham Tremper <gtremper@gmail.com>
2012-07-27 13:05:57 -0700

    fixed run kavosh

--- runKavosh.py
+++ runKavosh.py
@@ -7,5 +7,7 @@ total = str(len(files))
 for i,filename in enumerate(files):
 	print filename
 	print str(i)+" of "+total
+	if os.path.exists('output/'+filename):
+		continue
 	os.system("./Kavosh 6 "+filename)
 
