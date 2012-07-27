0ca77ca167b1c318215f52c2cf1f2dcec5fb32ae
Graham Tremper <gtremper@gmail.com>
2012-07-26 17:08:38 -0700

    added runKavosh

---
+++ runKavosh.py
@@ -0,0 +1,11 @@
+import os
+
+files = os.listdir("input")
+
+total = str(len(files))
+
+for i,filename in enumerate(files):
+	print filename
+	print str(i)+" of "+total
+	os.system("./Kavosh 4 "+filename)
+
