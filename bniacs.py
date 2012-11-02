4f90dd6eaf04a06cb9b41f7a9933ff95759f2232
jasonzliang <jasonzliang@berkeley.edu>
2012-11-02 02:58:12 -0700

    added way to print motifs

--- bniacs.py
+++ bniacs.py
@@ -2,3 +2,11 @@ from Bniacs_Motifs.FinalMotif import *
 from Bniacs_Parse_CSV.parseCsv import *
 from Bniacs_Network_Measures.nalz_test import *
 from Bniacs_Network_Measures.parseNewData import *
+
+def main():
+  with open("aznorbert_corrsd_new.pkl","rb") as f:
+    data = pickle.load(f)
+  findMotifs(data,('AD', 'corr'),motifSize=3,degree=10,randGraphs=None, useCache=True, printMotifs=True)
+  
+if __name__ == '__main__':
+  main()
